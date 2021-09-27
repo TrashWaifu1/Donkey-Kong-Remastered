99d68acb12b9e278e665afc74bb28a3a6a19d7e9
TrashWaifu1 <82532109+TrashWaifu1@users.noreply.github.com>
2021-09-27 10:24:48 -0700

    changed map and player controller

--- Donkey Kong Remastered/Assets/Scripts/PlayerController.cs
+++ Donkey Kong Remastered/Assets/Scripts/PlayerController.cs
@@ -4,24 +4,50 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public int Health = 3;
     public float speed = 10;
     public float JumpHight = 3;
-    public float Sensitivity = 50;
+    public float nockBack = 30;
     public Rigidbody RB;
+    public LayerMask groundMask;
+    public GameObject pos;
 
     Vector3 Velocity;
+    public bool isGrounded;
+    float Gravity = -9.81f * 3;
+    float groundDistace = 0.4f;
 
     void Update()
     {
-
+        #region Movement
+        #region directional movment
         Velocity = RB.velocity;
-        Velocity.z = Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
-        Velocity.x = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+        Velocity += (transform.right * Input.GetAxis("Horizontal") + transform.forward * Input.GetAxis("Vertical")) * speed * Time.deltaTime;
         RB.velocity = Velocity;
+        #endregion
+        #region Jump
+        isGrounded = Physics.CheckSphere(pos.transform.position, groundDistace, groundMask);
 
+        if (isGrounded && Velocity.y < 0)
+            Velocity.y = -2;
 
-        float MouseX = Input.GetAxis("Mouse X") * Sensitivity * Time.deltaTime;
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            Velocity.y = Mathf.Sqrt(JumpHight * -2f * Gravity);
+            RB.velocity = Velocity;
+        }
+        #endregion
+        #endregion
 
-        transform.Rotate(Vector3.up * MouseX);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Drum")
+        {
+            --Health;
+            Destroy(collision.transform.gameObject);
+            Velocity += Vector3.back * nockBack;
+        }
     }
 }
